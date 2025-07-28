f945186528ac2c59641f5f6cd1d36bf61eda787f
RTalaro <146395601+RTalaro@users.noreply.github.com>
2025-07-28 06:35:11 -0700

    feat: options
    - implement volume options
    - floating bubble in scene 1
    - restyle main menu
    - set icon

--- ui/menu.gd
+++ ui/menu.gd
@@ -1,12 +1,20 @@
 extends Control
 
+#preload all scenes
+var scene1 = preload("res://scenes/scene1.tscn").instantiate()
+#var scene2 = preload("res://scenes/scene2.tscn").instantiate()
+#var scene3 = preload("res://scenes/scene3.tscn").instantiate()
+#var scene4 = preload("res://scenes/scene4.tscn").instantiate()
+#var scene5 = preload("res://scenes/scene5.tscn").instantiate()
+#var scene6 = preload("res://scenes/scene6.tscn").instantiate()
+#var scene7 = preload("res://scenes/scene7.tscn").instantiate()
+
 @onready var play = $MarginContainer/VBoxContainer/Play
 @onready var options = $MarginContainer/VBoxContainer/Options
 @onready var quit = $MarginContainer/VBoxContainer/Quit
 
 
 func _ready():
-	
 	# connect all buttons
 	play.button_down.connect(on_start_button_pressed)
 	options.button_down.connect(on_options_button_pressed)
@@ -16,12 +24,10 @@ func _ready():
 func on_start_button_pressed() -> void:
 	print('start game')
 	print('load scene 1')
-	get_tree().change_scene_to_file("res://scenes/scene1.tscn")
+	add_child(scene1)
 
 func on_options_button_pressed() -> void:
-	print('start game')
-	print('load scene 1')
-	get_tree().change_scene_to_file("res://scenes/scene1.tscn")
+	print('open options')
 
 func on_quit_button_pressed() -> void:
 	print('quit game')
