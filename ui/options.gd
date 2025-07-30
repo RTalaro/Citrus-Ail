3c1762765701b042ff4d70eea1ed659d4e7037af
RTalaro <146395601+RTalaro@users.noreply.github.com>
2025-07-30 15:59:05 -0700

    fix: add exit button to menu options

--- ui/options.gd
+++ ui/options.gd
@@ -1,11 +1,19 @@
 extends Control
 
+@onready var exit = $MenuBG/TextureButton
+
 func _ready() -> void:
 	visibility_changed.connect(on_visibility_changed)
+	exit.button_down.connect(on_exit_button_down)
 
 func on_visibility_changed():
-	print("visibility changed")
 	get_tree().paused = !get_tree().paused
 	if get_parent().get_child_count() < 7:
-		$ColorRect.visible = false
-	else: $ColorRect.visible = true
+		$Dim.visible = false
+		$MenuBG.visible = true
+	else:
+		$Dim.visible = true
+		$MenuBG.visible = false
+
+func on_exit_button_down():
+	visible = !visible
